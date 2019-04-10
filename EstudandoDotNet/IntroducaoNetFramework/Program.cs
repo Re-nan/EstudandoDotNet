7b66acd9b93da7fcd90b9eedb9cf7b0ed8c076dd
Re-nan <renan.ps@outlook.com>
2019-04-09 22:43:24 -0300

    Adicionando estudos sobre Introdução ao .Net Framework

--- EstudandoDotNet/IntroducaoNetFramework/Program.cs
+++ EstudandoDotNet/IntroducaoNetFramework/Program.cs
@@ -10,7 +10,6 @@ namespace IntroducaoNetFramework
     {
         static void Main(string[] args)
         {
-
             /* Em ambiente do .Net Framework, não importa a linguagem que estamos
              * utilizando, seja C#, VB, C++. entre outras.... todo o nosso código
              * é compilado para o que chamamos de MSIL(Microsoft Intermediate Language)
@@ -26,9 +25,9 @@ namespace IntroducaoNetFramework
              * ----- Entendo com mais detalhes -----
              * 
              * Você escreve seu código em C#
-             * Console.WriteLine("Olá Mundo!")
+             * Console.WriteLine("Olá Mundo!");
              * 
-             * E a MSIL converte seu código que um humano entende para para uma linguagem 
+             * E a MSIL converte seu código que um humano entende para uma linguagem 
              * intermediária que só a CLR(máquina virtual) entende
              * nop
              * ldstr "Olá Mundo!"
@@ -38,6 +37,34 @@ namespace IntroducaoNetFramework
              * 
              * E a CLR pega esse código acima e converte para linguagem de máquina para que 
              * o seu processador Intel ou AMD possa entender e executar sua aplicação
+             */
+
+
+
+            /* O .Net Framework vem por padrão instalado no Windows, cada Windows a partir do
+             * "Windows XP" já vem com uma versão do .Net Framework instalada
+             * 
+             * E ele fica na pasta C:\Windows\Microsoft.NET e dentro desse diretório eu tenho
+             * os diretórios "Framework" que representa o 32x e o diretório "Framework64" que 
+             * representa o 64x, essa escolha depende da aplicação que eu quero executar
+             * 
+             * Dentro do diretório Framework/Framework64 eu tenho outros diretórios que 
+             * representam a versão do .net framework e cada diretório de versão possui os
+             * arquivos necessários para a execução de uma aplicação feita para a plataforma do
+             * .net framework, que vão de bibliotecas e arquivos usados pela CLR, um desses arquivos
+             * é o "csc.exe" que é compilador do C#
+             * 
+             * Com isso é possível escrever C# em uma sistema Windows sem precisar ter uma IDE
+             * como o Visual Studio, basta escrever nosso código em um bloco de notas e salvarmos
+             * ele como txt mesmo
+             * 
+             * Abrir o cmd e navegar até o diretório C:\Windows\Microsoft.NET\Framework\v4.0.30319
+             * e executar o csc.exe seguindo do nome do arquivo a ser compilado
+             * C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe MeuPrograma.txt
+             * 
+             * O C# irá gerar na mesma pasta do MeuPrograma.txt um arquivo MeuPrograma.exe que ao
+             * exercutarmos rodará nosso programa para a plataforma .Net Framework em ambiente Windows
+             * 
              */
 
         }
