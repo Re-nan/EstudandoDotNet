c3b5e1f65504953efb1e1be0cb1884f4131013ec
rsanchez <renan.sanchez@mc1.com.br>
2020-03-23 20:45:41 -0300

    Adicionando estudos sobre Sobrecarga

---
+++ EstudandoDotNet/EstudandoSobrecarga/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudandoSobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result;
+
+            Matematica mat = new Matematica();
+
+            result = mat.Somar(5, 10);
+            Console.WriteLine(result);
+
+            /* O C# sabe que não tem um método que receba um double e um int
+             * então ele procura um método que se encaixe fazendo o cast implícito do valor
+             */ 
+            result = mat.Somar(5.5, 10);
+            Console.WriteLine(result);
+
+
+            result = mat.Somar(5,5,5,5,5,5);
+            Console.WriteLine(result);
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
