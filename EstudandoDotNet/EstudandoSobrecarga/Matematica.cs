1002e80ae0ac9664954ee208b7941e750d5bfffb
re-nan <renan.ps@outlook.com>
2020-03-24 00:24:40 -0300

    Adicionando estudos sobre Sobrecarga

---
+++ EstudandoDotNet/EstudandoSobrecarga/Matematica.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* SOBRECARGA DE MÉTODOS
+ * É quando você tem métodos com o mesmo nome, mas com parametros diferentes, podendo ter a mesma
+ * quantidade de parametros, desde que os tipos sejam diferentes
+ * 
+ * A CLR do .net se da o trabalho de encaixar a versão do método que melhor atendar os parâmetros
+ * 
+ * ATENÇÃO
+ * Se você tiver 2 métodos com o mesmo nome e mesma quantidade e tipos de de parametros
+ * mas o retorno diferente, a CLR dará erro, pois o 
+ * 
+ * A CLR do .net se da o trabalho de encaixar a versão do método que melhor atendar os parâmetros
+ */
+namespace EstudandoSobrecarga
+{
+    class Matematica
+    {
+
+        public double Somar(long n1, long n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Somar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Somar(double n1, double n2, double n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        public double Somar(params double[] values)
+        {
+            double result = 0;
+            foreach (double item in values)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+    }
+}
--- EstudandoDotNet/EstudandoSobrecarga/Program.cs
+++ EstudandoDotNet/EstudandoSobrecarga/Program.cs
@@ -10,7 +10,26 @@ namespace EstudandoSobrecarga
     {
         static void Main(string[] args)
         {
+            double result;
+
+            Matematica mat = new Matematica();
+
+            result = mat.Somar(5, 10);
+            Console.WriteLine(result);
+
+            /* O C# sabe que não tem um método que receba um double e um int
+             * então ele procura um método que se encaixe fazendo o cast implícito do valor
+             */
+            result = mat.Somar(5.5, 10);
+            Console.WriteLine(result);
+
+
+            result = mat.Somar(5, 5, 5, 5, 5, 5);
+            Console.WriteLine(result);
+
+
             Console.ReadKey();
+
         }
     }
 }
