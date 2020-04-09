4eb9775baa8ad0901c5ac0aa86b389ef67dbfc70
re-nan <renan.ps@outlook.com>
2020-04-09 00:26:26 -0300

    Adicionando estudos sobre Construtores

---
+++ EstudandoDotNet/EstudandoConstrutores/Livro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudandoConstrutores
+{
+    class Livro
+    {
+        public int numPaginas;
+        public string titulo;
+        public string autor;
+
+        /* Um construtor pode chamar outro através do operador this
+         * Quando você chamar Livro(titulo, autor), primeiro será chamado
+         * o Livro(titulo) e só depois o compilador irá retornar
+         * e chamar o Livro(titulo, autor)
+         */
+
+        //Costrutor Padrão
+        public Livro() { }
+
+        //Sobrecarga de Construtor
+        public Livro(string titulo) 
+        {
+            numPaginas = 100;
+        }
+
+        //Construtor Sobrecarregado
+        public Livro(string titulo, string autor) : this(titulo)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+        }
+
+    }
+}
---
+++ EstudandoDotNet/EstudandoConstrutores/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Valor padrão assúmido dos tipos primitivos
+ * 
+ * int, short, byte, long, float, double = 0
+ * bool = false
+ * char = '\0' (Reprezenta nada)
+ * 
+ * Value types assumem o valor padrão
+ * Reference types(Class Pessoa) assumem = null
+ */
+
+/* Construtor
+ * - é um método da POO que é chamado no momento da inicialização/instanciação
+ * daquele obj, é o momento que você tem para inicializar seus Fields antes de
+ * usar aquele obj
+ * 
+ * - Possuem o mesmo nome do método
+ * - Não possuem tipo de retorno
+ * - podem conter ou não parâmetros
+ * - Assim como método podem conter sobrecarga de construtores
+ * 
+ * Uma classe sempre deve te rum construtor definido na classe
+ * se não não criar esse construtor o compilador cria o construtor
+ * padrão de forma implícita
+ */
+ 
+namespace EstudandoConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Este Contrutor inicializou os fields da classe
+             * com valores padrões 
+             * titulo = ""
+             * autor = ""
+             * numPaginas = 0
+             */
+            Livro livro1 = new Livro();
+
+            Console.WriteLine("Título: " + livro1.titulo);
+            Console.WriteLine("Autor: " + livro1.autor);
+            Console.WriteLine("Num Páginas: " + livro1.numPaginas);
+
+            Console.WriteLine();
+            
+            /* Este Contrutor inicializou os fields da classe com valores
+             * definidos no momento de sua criação
+             */ 
+            Livro livro2 = new Livro("Game Of Thrones", "George R. R. Martin");
+
+            Console.WriteLine("Título: " + livro2.titulo);
+            Console.WriteLine("Autor: " + livro2.autor);
+            Console.WriteLine("Num Páginas: " + livro2.numPaginas);
+
+            Console.ReadKey();
+        }
+    }
+}
