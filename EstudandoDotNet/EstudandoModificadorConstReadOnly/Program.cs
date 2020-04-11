fa489c0a83f150c488c2d3bee4e397c87e94600e
re-nan <renan.ps@outlook.com>
2020-04-10 21:52:32 -0300

    Adicionando estudos sobre modificadores const e readonly

---
+++ EstudandoDotNet/EstudandoModificadorConstReadOnly/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Modificador const 
+ * - Impede que o field possa ter seu valor alterado
+ * - fields declarado como const são implicitamente static também
+ * - Precisam terem seu valor setados no momento de sua criação
+ */
+
+/* Modificador readonly
+ * Diferente de const o modificador readonly não define o elemento como
+ * static
+ * Não precisam terem seu valor inicializado no momento de sua criação
+ * Mas após setaod um valro este não pode ser alterado
+ */
+
+/* Tanto um como o outro não pode ter seu valor alterado */ 
+
+namespace EstudandoModificadorConstReadOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PI: " + Matematica.PI);
+
+            Matematica m = new Matematica();
+            Console.WriteLine("E: " + m.E);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Matematica
+    {
+        /* PI
+         * não pode ter sue valor alterado
+         * é do tipo static implicitamente
+         * pertence a classe e não a instancia
+         */
+        public const double PI = 3.1416;
+
+
+        /* Número de Euler
+         * pertence a instancia, porém se adc o static, pertence somente a classe
+         */
+        public readonly double E = 2.71828;
+
+    }
+
+}
