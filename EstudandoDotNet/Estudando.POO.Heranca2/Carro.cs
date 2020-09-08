9d2974a0a994175d71582cc8879ede9daf9a885f
Re-nan <renan.ps@outlook.com>
2020-09-07 22:26:18 -0300

    Adicionando estudos sobre Herança

---
+++ EstudandoDotNet/Estudando.POO.Heranca2/Carro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudando.Heranca2
+{
+    class Carro : IMotorizado
+    {
+        // prop da classe Carro
+        public string NomeCarro { get; set; }
+
+        // prop herdada da interface
+        public string NomeMotor { get; set; }
+
+        // implementando método da interface
+        public void Ligar()
+        {
+            Console.WriteLine("Carro ligado");
+        }
+
+        // Sobrescrevendo método ToString() da classe Object
+        public override string ToString()
+        {
+            return $"Carro: {NomeCarro} \nMotor: {NomeMotor}"; 
+        }
+    }
+}
---
+++ EstudandoDotNet/Estudando.POO.Heranca2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Estudando.Heranca2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Interfaces
+ * - Assim como classes, intrfaces também são tipos de dados. Em interfaces temos apenas
+ * assinaturas de métodos ou properties. Nada é implementado, fields e constutores não são
+ * permitidos
+ * - Tudo o que você declara em uma interface é por padrão public, definido implicitamente
+ * - O conceito de interface é focar no que o objeto faz e não como eles faz!
+ * - As classes que implementam uma interfaces se tornam do tipo da interface, assim como
+ * classes, também usamos o termo "É UM". Porém cuidado com o excesso de "É UM" em sua hierarquia
+ * e acabar engessando as classes. opte pela associação "TEM UM" em que uma classe é composta
+ * de outras classes ou interfaces
+ */
+
+namespace Estudando.POO.Heranca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro();
+            c.NomeCarro = "Jeep Renegade";
+            c.NomeMotor = "2.0 Multijet II";
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
