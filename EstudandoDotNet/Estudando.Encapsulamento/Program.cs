56cdf3929c57b2b86097c24eb54a12a68683df4a
Re-nan <renan.ps@outlook.com>
2020-09-17 21:18:10 -0300

    Adicionando estudos sobre Encapsulamento

---
+++ EstudandoDotNet/Estudando.Encapsulamento/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* Devemos usar o encapsulamento sempre que pudermos esconder fields e detalhes
+ * de implementação
+ */
+ 
+namespace Estudando.POO.Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno("Renan");
+            Console.WriteLine(a1.Nome);
+
+            //idade recebe zero porque o cosntrutor lá em cima chamou o construtor padrão pra ela implicitamente
+            a1.Nome = "Novo renan";
+            Console.WriteLine(a1.Nome + " " + a1.Idade);
+
+            a1.Idade = 28;
+            Console.WriteLine(a1.Nome + " " + a1.Idade);
+
+            //Outra forma de setar valores
+            Aluno a2 = new Aluno("Danielle")
+            {
+                Idade = 24
+            };
+            Console.WriteLine("\n" + a2.Nome + " " + a2.Idade);
+
+            Console.ReadKey();
+        }
+    }
+}
