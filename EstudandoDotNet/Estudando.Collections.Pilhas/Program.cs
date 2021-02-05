887a7cd095964ef2479caf27c4f1d12eb7b19830
Re-nan <renan.ps@outlook.com>
2021-02-04 22:46:17 -0300

    Adicionando estudos sobre Pilhas

---
+++ EstudandoDotNet/Estudando.Collections.Pilhas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudando.Collections.Pilhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando pilha
+            Stack<char> pilha = new Stack<char>();
+
+            // Empilhando
+            pilha.Push('A');
+            pilha.Push('B');
+            pilha.Push('C');
+
+            /* Desimpilhando
+             * Retorna o primeiro da pilha, mas sem deixar ele lá, remove da pilha
+             * se a pilha estiver vazia ocorre um InvalidOperationException
+             */
+            char ultimo = pilha.Pop();
+            Console.WriteLine(ultimo);
+
+            // Conta a qtd de elementos
+            pilha.Count();
+
+            /* Retorna o primeiro da pilha, mas deixa ele lá ainda, não remove ele
+             * se a pilha estiver vazia ocorre um InvalidOperationException
+             */
+            ultimo = pilha.Peek();
+            Console.WriteLine(ultimo);
+
+
+            Console.ReadKey();
+        }
+    }
+}
